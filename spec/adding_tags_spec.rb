793a9dcbb65656472827612cb2d69cf216673f72
RachelNolan <mail@rachelnolan.com>
2014-10-07 13:29:47 +0100

    Can add tags to bookmarks from web.

---
+++ spec/adding_tags_spec.rb
@@ -0,0 +1,19 @@
+# require 'spec_helper'
+	
+# 	scenario "with a few tags" do 
+
+# 	visit "/"
+# 	add_link("http://www.makersacademy.com/", 
+# 			"Makers Academy", 
+# 			['education', 'ruby'])
+# 	link = Link.first
+# 	expect(link.tags).to_include('education')
+# 	expect(link.tags).to_include('ruby')
+
+
+# 	scenario "with a few tags" do 
+
+
+
+
+# end
